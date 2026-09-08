f329059a14400c4eeaa0086871c75d5a2e1e67b9
waslla <wasibulislam8984@gmail.com>
2026-09-09 04:49:06 +0600

    App make old

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,13 +4,10 @@ import Nav from "./Components/Nav/Nav";
 import { PALETTE } from "./Theme/palette";
 import { useGetName } from "./Hooks/userGetAppName";
 import { Bounce, ToastContainer } from "react-toastify";
-import AnimatedPage from "./Components/Common/AnimatedPage";
 
 const App = () => {
   return (
-    <div className="app-shell relative w-full min-h-screen overflow-hidden">
-      <div className="app-orb app-orb-one" aria-hidden="true" />
-      <div className="app-orb app-orb-two" aria-hidden="true" />
+    <div className=" w-full">
       <Helmet>
         <title>{useGetName}</title>
       </Helmet>
@@ -31,9 +28,9 @@ const App = () => {
         transition={Bounce}
       />
 
-      <div className="relative z-10 ml-0 pt-16 md:ml-[98px] md:pt-5 min-h-screen flex flex-col">
+      <div className="ml-auto mt-15 md:mt-auto md:ml-[98px] md:pt-5 h-[calc(100vh*86/100)] md:h-[calc(100vh-20px)] flex flex-col">
         <div
-          className="flex-1 min-h-0 mx-0 sm:mx-1 md:mr-5 p-2 sm:p-3 md:p-5 rounded-2xl rounded-t-none md:rounded-t-2xl md:rounded-l-none flex flex-col"
+          className="flex-1 min-h-0 mx-2 md:mr-5 p-2 rounded-2xl rounded-t-none md:rounded-t-2xl md:rounded-l-none flex flex-col"
           style={{
             backgroundColor: PALETTE.bg,
             borderRight: `1px solid ${PALETTE.steel}`,
@@ -41,10 +38,8 @@ const App = () => {
             cursor: "pointer",
           }}
         >
-          <div           className="flex-1 min-h-0 flex flex-col min-w-0 px-1 sm:px-2 md:px-3 py-2 sm:py-3 md:py-4 transition-all duration-300 ease-in-out bg-gray-300/30 rounded-xl rounded-t-none md:rounded-t-xl md:rounded-l-none overflow-x-auto overflow-y-auto">
-            <AnimatedPage>
-              <Outlet />
-            </AnimatedPage>
+          <div className="flex-1 min-h-0 flex flex-col px-1 md:px-3 py-2 md:py-4 transition-all duration-300 ease-in-out  bg-gray-300/30 rounded-xl rounded-t-none md:rounded-t-xl md:rounded-l-none overflow-scroll">
+            <Outlet />
           </div>
         </div>
       </div>
